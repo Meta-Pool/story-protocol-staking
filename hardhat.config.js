9ec4601aa9d1a6638e9745d1f96f4e5c1191773c
leanonchain <leandroezeleguizamon@gmail.com>
2025-01-31 11:32:59 -0300

    Add localhost to hardhat.config

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,6 +12,9 @@ require("hardhat-contract-sizer");
 module.exports = {
   defaultNetwork: 'hardhat',
   networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
     hardhat: {
       // chainId: FORKING_CHAIN_ID,
       // accounts: { mnemonic: MNEMONIC },
