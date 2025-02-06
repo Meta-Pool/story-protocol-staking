5659972438d816c6e4a8cf34a1299974412225fa
leanonchain <leandroezeleguizamon@gmail.com>
2025-02-06 18:49:57 -0300

    Remove old interface IStakedIPVaultOperations

--- contracts/interfaces/IStakedIPVaultOperations.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.28;
-
-interface IStakedIPVaultOperations {
-    function getValidatorIndex(bytes memory _validatorUncmpPubkey) external view returns (uint256);
-
-    function isValidatorListed(bytes memory _validatorUncmpPubkey) external view returns (bool);
-}
