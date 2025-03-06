c7b93f22d2ed40cc1bf6028a423d68b04c09e086
leanonchain <leandroezeleguizamon@gmail.com>
2025-03-05 21:50:32 -0300

    Add upgrade_staked_ip

---
+++ scripts/upgrade_staked_ip.js
@@ -0,0 +1,23 @@
+const { ethers, upgrades } = require("hardhat");
+
+async function main() {
+    const provider = ethers.provider
+    const owner = new Wallet(process.env.OWNER_PRIVATE_KEY, provider);
+
+    const PROXY_ADDRESS = "0xd07Faed671decf3C5A6cc038dAD97c8EFDb507c0";
+
+    const NewStakedIP = await ethers.getContractFactory("StakedIP", owner);
+
+    console.log("Deploying new implementation and upgrading proxy for StakedIP");
+
+    await upgrades.upgradeProxy(PROXY_ADDRESS, NewStakedIP, { signer: owner });
+
+    console.log(`Proxy upgraded successfully. New implementation at: ${await upgrades.erc1967.getImplementationAddress(PROXY_ADDRESS)}`);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
